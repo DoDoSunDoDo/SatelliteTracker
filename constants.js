a545f8d2853c6836058d4be906ffd536e769c330
DoDoSunDoDo <xfsun0114@gmail.com>
2021-10-31 20:50:03 -0700

    sate
    sate

---
+++ constants.js
@@ -0,0 +1,7 @@
+export const SAT_API_KEY = "LQ9TX9-8QH9T3-488TN3-4SHJ";
+
+export const STARLINK_CATEGORY = "52";
+
+export const NEARBY_SATELLITE = "rest/v1/satellite/above";
+export const WORLD_MAP_URL = "https://unpkg.com/world-atlas@1/world/110m.json";
+export const SATELLITE_POSITION_URL = "rest/v1/satellite/positions";
